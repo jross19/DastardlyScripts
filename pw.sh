49e8b8f4706e875ed7964d7414c7f8788a2b9153
jross19 <jross19@stjohnsprep.org>
2019-01-30 14:14:19 -0500

    yeet

--- pw.sh
+++ pw.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Script accept password using read commnad
-# Not *very secure*, this script is for learning purpose only
-# -------------------------------------------------------------------------
-# This script is part of nixCraft shell script collection (NSSC)
-# Visit http://bash.cyberciti.biz/ for more information.
-# -------------------------------------------------------------------------
 PASS="abc123"
 read -s -p "Password: " mypassword
 echo ""
